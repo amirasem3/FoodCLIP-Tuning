6417e22085f2f35c247f8d0f705841dcdd530b4b
Asem <asem@Asems-MacBook-Pro.local>
2026-01-24 15:12:21 +0300

    Prompt: Adding the different prompt for getting an image of a food

--- src/prompts.py
+++ src/prompts.py
@@ -1,4 +1,13 @@
 """
 Prompts samples like "a photo of a {}."
-
 """
+TEMPLATES = [
+    "a photo of a {}.",
+    "a close-up photo of a {}.",
+    "a bright photo of a {}.",
+    "a cropped photo of a {}.",
+    "a blurry photo of a {}.",
+    "a photo of the food {}.",
+    "a delicious photo of {}.",
+    "a photo of a plate of {}.",
+]
